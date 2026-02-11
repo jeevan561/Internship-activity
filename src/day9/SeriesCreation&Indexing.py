0015106fa1c6bfd9b52ba4d2cbe4e853a81b1334
jeevan561 <jeevank5106@gmail.com>
2026-02-11 12:55:47 +0530

    day9 uploaded

---
+++ src/day9/SeriesCreation&Indexing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+products = pd.Series(
+    [700, 150, 300],
+    index=['Laptop', 'Mouse', 'Keyboard']
+)
+
+laptop_price = products['Laptop']
+
+first_two = products.iloc[:2]
+
+print("Full Series:")
+print(products)
+
+print("\nPrice of Laptop:")
+print(laptop_price)
+
+print("\nFirst Two Products:")
+print(first_two)
---
+++ src/day9/Vectorized String Operations.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+usernames = pd.Series([' Alice ', 'bOB', ' Charlie_Data ', 'daisy'])
+
+cleaned_usernames = usernames.str.strip().str.lower()
+
+contains_a = cleaned_usernames.str.contains('a')
+
+print("Cleaned Usernames:")
+print(cleaned_usernames)
+
+print("\nNames containing letter 'a':")
+print(contains_a)
